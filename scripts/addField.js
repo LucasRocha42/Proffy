86768a4d7f7ea0ff772a9327d6da9aa07da53d02
lucasrocha42 <lucasfariascr1@gmail.com>
2022-01-20 09:06:43 -0200

    Javascript

---
+++ scripts/addField.js
@@ -0,0 +1,18 @@
+// Procura o botão 
+document.querySelector("#add-time")
+// Quando clicar no botão
+.addEventListener('click', cloneField )
+// Executar uma ação 
+function cloneField() {
+    // Duplicar os campos. Que campos?
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+    // pegar os campos. Que campos?
+    const fields = newFieldContainer.querySelectorAll('input')    
+    // para cada campo. limpar
+    fields.forEach(function(field) {
+        // pega o field do momento e limpa ele 
+        field.value = ""
+    })
+    // Colocar na pagina: onde?
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
